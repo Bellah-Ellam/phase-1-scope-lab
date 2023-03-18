918aa49fd7828ef72babd33755b944d2e6a3e8fb
Bellah Ellam <bella.ellam@student.moringaschool.com>
2023-03-18 21:31:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+
+
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+  }
+
+// console.log(upperCaseCustomerName())
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+
+  function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+  }
+
+  const leastFavoriteCustomer = 'ogada'
+  
+  function changeLeastFavoriteCustomer(newCustomer) {
+    leastFavoriteCustomer = newCustomer; // This will result in an error!
+  }
